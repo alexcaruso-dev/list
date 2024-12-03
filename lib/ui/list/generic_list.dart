12dadee73135a6af086a9efae555a77763b091d1
alex <alexcaruso.dev@gmail.com>
2024-12-02 22:21:11 -0500

    Launch bottom sheet after tapping FAB

--- lib/ui/list/generic_list.dart
+++ lib/ui/list/generic_list.dart
@@ -12,9 +12,25 @@ class _GenericListState extends State<GenericList> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: show,
           child: const Icon(Icons.add)
       ),
     );
+  }
+
+  void show() {
+    showModalBottomSheet(
+      context: context,
+        builder: (context) => Padding(
+          padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom
+          ),
+          child: const SizedBox(
+            height: 200, // Set your desired height
+            child: Center(
+              child: TextField(autofocus:true),
+            ),
+          )
+        ));
   }
 }
