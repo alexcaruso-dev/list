549b0f8d30850bfaaf9db863b40b8657fe4461f7
alex <alexcaruso.dev@gmail.com>
2024-12-14 21:35:10 -0500

    Add item to list when submitted and clear text field

--- lib/ui/list/generic_list.dart
+++ lib/ui/list/generic_list.dart
@@ -8,35 +8,51 @@ class GenericList extends StatefulWidget {
 }
 
 class _GenericListState extends State<GenericList> {
+  final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+  final List<String> _items = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-          onPressed: show,
+          onPressed: _show,
           child: const Icon(Icons.add)
       ),
     );
   }
 
-  void show() {
+  void _show() {
     showModalBottomSheet(
       context: context,
-        builder: (context) => Padding(
-          padding: EdgeInsets.only(
-              bottom: MediaQuery.of(context).viewInsets.bottom
-          ),
-          child: const SizedBox(
-            width: double.infinity,
-            child: TextField(
-              autofocus: true,
-              maxLines: null,
-              decoration: InputDecoration(
-                labelText: 'Add a new item',
-                contentPadding: EdgeInsets.all(20.0),
+      builder: (context) => Padding(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        child: SizedBox(
+          width: double.infinity,
+          child: TextField(
+            controller: _controller,
+            focusNode: _focusNode,
+            autofocus: true,
+            maxLines: null,
+            textInputAction: TextInputAction.done,
+            decoration: const InputDecoration(
+              labelText: 'Add a new item',
+              contentPadding: EdgeInsets.all(20.0),
             ),
+            onSubmitted: _addItem,
           ),
         ),
       )
     );
+  }
+
+  void _addItem(String item) {
+    setState(() {
+      _items.add(item);
+      _controller.clear();
+      _focusNode.requestFocus();
+    });
   }
 }
